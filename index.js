b9a286c9e2084405aec341608c926f759eca6630
Iyiola-am <adekunle3317@gmail.com>
2020-08-05 21:01:42 +0100

    Implemented basic views

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const handlebars = require('express3-handlebars');
 
 // Configure app.
 dotenv.config();
 const app = express();
 app.set('port', process.env.PORT || 8080);
+app.engine('handlebars', handlebars.create({ defaultLayout:'main' }).engine);
+app.set('view engine', 'handlebars');
 
 
 app.get('/', function(req, res){
-    res.type('text/plain');
-    res.send('Hello world!');
+    res.render('index');
 });
 
 
 // custom 404 page
 app.use(function(req, res, next){
-    res.type('text/plain');
     res.status(404);
-    res.send('404 - Not Found');
+    res.render('404');
 });
 
 // custom 500 page
 app.use(function(err, req, res, next){
     console.error(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 });
 
 app.listen(app.get('port'), () => {
