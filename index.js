b568ebd69569bf2fb7a60a66f03b9a21747fc878
Iyiola-am <adekunle3317@gmail.com>
2020-08-05 20:36:02 +0100

    Basic server implemented

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+// Configure app.
+dotenv.config();
+const app = express();
+app.set('port', process.env.PORT || 8080);
+
+
+
+// custom 404 page
+app.use(function(req, res){
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not Found');
+});
+
+// custom 500 page
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.type('text/plain');
+    res.status(500);
+    res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), () => {
+    console.log(`App running on http://localhost:${process.env.PORT}`);
+});
