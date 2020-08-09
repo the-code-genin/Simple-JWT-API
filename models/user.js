db72cbc66b990e516c38dfc56e369257524f25c1
Iyiola-am <adekunle3317@gmail.com>
2020-08-09 03:22:23 +0100

    Updated user model

--- models/user.js
+++ models/user.js
@@ -2,11 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Model = mongoose.Model;
 
-const UserModel = new Model('User', new Schema({
+const UserSchema = new Schema({
     email:  String,
     password: String,
     created_at: { type: Date, default: Date.now },
     updated_at: { type: Date, default: Date.now },
-}));
+});
+
+
+const UserModel = new Model('User', UserSchema);
 
 module.exports = UserModel;
