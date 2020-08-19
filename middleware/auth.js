b5b1dfd42533e027e4aeb5a37ca4d8bf94ddc425
Iyiola-am <adekunle3317@gmail.com>
2020-08-19 13:55:22 +0100

    Refactored auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,8 +2,9 @@ const jwt = require('../lib/jwt');
 const UserModel = require('../models/user');
 
 module.exports = function(req, res, next) {
-    let id = jwt.verifyAccessToken(req.get('Authorization'));
-    if (!id) {
+    let header = req.get('Authorization');
+
+    if (!/^Bearer (.+)$/i.test(header)) {
         res.json({
             success: false,
             error: {
@@ -13,20 +14,36 @@ module.exports = function(req, res, next) {
             }
         });
     } else {
-        UserModel.findOne({_id: id}).exec().then(user => {
-            if (user == null) {
+        let token = /^Bearer (.+)$/i.exec(header)[1].trim();
+        let id = jwt.verifyAccessToken(token);
+
+        if (!id) {
+            res.json({
+                success: false,
+                error: {
+                    code: 401,
+                    type: 'AuthenticationError',
+                    message: 'User is not Authenticated'
+                }
+            });
+        } else {
+            UserModel.findOne({_id: id, auth_tokens: {$ne: token}}).select('-password -auth_tokens').exec().then(user => {
+                if (user == null) {
+                    throw new Error('User is not Authenticated');
+                } else {
+                    req.user = user;
+                    next();
+                }
+            }).catch(e => {
                 res.json({
                     success: false,
                     error: {
                         code: 401,
                         type: 'AuthenticationError',
-                        message: 'User is not Authenticated'
+                        message: e.message
                     }
                 });
-            } else {
-                req.user = user;
-                next();
-            }
-        });
+            });
+        }
     }
 }
