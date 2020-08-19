f22bba400cae5bd0ad4cb45abd4008ebe7f695a6
Iyiola-am <adekunle3317@gmail.com>
2020-08-19 13:10:38 +0100

    Refactored code base

--- models/user.js
+++ models/user.js
@@ -3,10 +3,25 @@ const Schema = mongoose.Schema;
 const Model = mongoose.model;
 
 const UserSchema = new Schema({
-    email:  String,
-    password: String,
-    created_at: { type: Date, default: Date.now },
-    updated_at: { type: Date, default: Date.now },
+    email:  {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    auth_tokens: [String],
+    created_at: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    updated_at: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
 });
 
 UserSchema.methods.toJSON = function() {
