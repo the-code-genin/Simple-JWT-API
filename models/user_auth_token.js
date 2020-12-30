0c3b7219c19c45be8c7d692b0e72b7a7fa0844bc
Iyiola-am <adekunle3317@gmail.com>
2020-12-30 15:43:43 +0100

    Refactored user auth tokens model

--- models/user_auth_token.js
+++ models/user_auth_token.js
@@ -3,4 +3,7 @@ const db = require('../db');
 module.exports = db.model('UserAuthToken', {
     tableName: 'user_auth_tokens',
     hasTimestamps: true,
+    user() {
+      return this.belongsTo('User')
+    }
 });
