1a405cbf79fc7fc7a2f965e71df015fd9fc072af
Iyiola-am <adekunle3317@gmail.com>
2020-12-30 15:16:38 +0100

    Added db implementation to app

---
+++ db.js
@@ -0,0 +1,14 @@
+const db = require('knex')({
+    client: 'mysql',
+    connection: {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    }
+});
+
+const orm = require('bookshelf')(db);
+
+module.exports = orm;
