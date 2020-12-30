e494c98678f8cae0ca11f18615f9df140b9c4650
Iyiola-am <adekunle3317@gmail.com>
2020-12-30 15:41:32 +0100

    Refactored models

--- models/user.js
+++ models/user.js
@@ -2,6 +2,8 @@ const db = require('../db');
 
 module.exports = db.model('User', {
     tableName: 'users',
+    hasTimestamps: true,
+    hidden: ['password'],
     authTokens() {
         return this.hasMany('UserAuthToken');
     }
--- models/user_auth_token.js
+++ models/user_auth_token.js
@@ -2,4 +2,5 @@ const db = require('../db');
 
 module.exports = db.model('UserAuthToken', {
     tableName: 'user_auth_tokens',
+    hasTimestamps: true,
 });
