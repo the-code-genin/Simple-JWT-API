026b18d9aa31d42b087dc8f3987aa9c6793b4ad7
Iyiola-am <adekunle3317@gmail.com>
2020-12-30 14:43:04 +0100

    Implemented knex migratinos

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = async () => {
+  return {
+    client: 'mysql',
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    }
+  }
+};
