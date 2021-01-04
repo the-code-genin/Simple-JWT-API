233577b39b0eb8760f929b6793687c84412c9405
Iyiola-am <adekunle3317@gmail.com>
2021-01-05 00:16:31 +0100

    Refactored base model

--- models/base-model.ts
+++ models/base-model.ts
@@ -1,17 +1,14 @@
 // @ts-nocheck
 import { Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, BaseEntity } from "typeorm";
-import MomentDateString from "../lib/decorators/moment-date-string";
 
 @Entity()
 export default abstract class BaseModel extends BaseEntity {
     @PrimaryGeneratedColumn('increment')
     id: number;
 
-    @MomentDateString
     @CreateDateColumn()
     created_at: String;
 
-    @MomentDateString
     @UpdateDateColumn()
     updated_at: String;
 }
