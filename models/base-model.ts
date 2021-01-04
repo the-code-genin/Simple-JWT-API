781ec1d66e3ef56c7dced00efe8125e2c0800140
Iyiola-am <adekunle3317@gmail.com>
2021-01-05 00:14:55 +0100

    Implemented base model

--- models/base-model.ts
+++ models/base-model.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, BaseEntity } from "typeorm";
 import MomentDateString from "../lib/decorators/moment-date-string";
 
@@ -13,11 +14,4 @@ export default abstract class BaseModel extends BaseEntity {
     @MomentDateString
     @UpdateDateColumn()
     updated_at: String;
-
-    constructor(id: number, created_at: string, updated_at: string) {
-        super();
-        this.id = id;
-        this.created_at = created_at;
-        this.updated_at = updated_at;
-    }
 }
