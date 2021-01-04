f864846489f0c1adf60475e3ed20ce6231d1d163
Iyiola-am <adekunle3317@gmail.com>
2021-01-05 00:54:56 +0100

    Refactored user model

---
+++ models/user.ts
@@ -0,0 +1,18 @@
+import BaseModel from './base-model'
+import { Column, Entity, OneToMany } from 'typeorm';
+import { UserAuthToken } from './user_auth_token';
+
+@Entity()
+export class User extends BaseModel {
+    @Column('text')
+    // @ts-ignore
+    email: string;
+
+    @Column('text')
+    // @ts-ignore
+    password: string;
+
+    @OneToMany(type => UserAuthToken, (userAuthToken: UserAuthToken) => userAuthToken.user)
+    // @ts-ignore
+    userAuthTokens: UserAuthToken[]
+}
