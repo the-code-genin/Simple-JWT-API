fab6ecb88afd88d26e347fd559c0bb17a3a3d951
Iyiola-am <adekunle3317@gmail.com>
2021-01-04 19:31:56 +0100

    Converted db init file to typescript

--- db.ts
+++ db.ts
@@ -1,14 +1,15 @@
-const db = require('knex')({
+import knex from 'knex'
+import bookshelf from 'bookshelf'
+
+const db = knex({
     client: 'mysql',
     connection: {
         host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
+        port: Number(process.env.DB_PORT),
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
     }
 });
 
-const orm = require('bookshelf')(db);
-
-module.exports = orm;
+export default bookshelf(db);
