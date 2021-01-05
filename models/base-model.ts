867318fc2df0f71bad1a6e8ce47674427565861b
Iyiola-am <adekunle3317@gmail.com>
2021-01-05 01:44:13 +0100

    refactored base model

--- models/base-model.ts
+++ models/base-model.ts
@@ -1,7 +1,6 @@
 // @ts-nocheck
-import { Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, BaseEntity } from "typeorm";
+import { PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, BaseEntity } from "typeorm";
 
-@Entity()
 export default abstract class BaseModel extends BaseEntity {
     @PrimaryGeneratedColumn('increment')
     id: number;
