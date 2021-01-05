bf1887da4b51e981425bfb0731bf576b5a737715
Iyiola-am <adekunle3317@gmail.com>
2021-01-05 01:51:43 +0100

    Refactored user model

--- models/user.ts
+++ models/user.ts
@@ -1,9 +1,20 @@
-import BaseModel from './base-model'
-import { Column, Entity, OneToMany } from 'typeorm';
+import { PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, Column, Entity, BaseEntity, OneToMany } from 'typeorm';
 import UserAuthToken from './user_auth_token';
 
 @Entity('users')
-export default class User extends BaseModel {
+export default class User extends BaseEntity {
+    @PrimaryGeneratedColumn('increment')
+    // @ts-ignore
+    id: number;
+
+    @CreateDateColumn()
+    // @ts-ignore
+    created_at: String;
+
+    @UpdateDateColumn()
+    // @ts-ignore
+    updated_at: String;
+
     @Column('text')
     // @ts-ignore
     email: string;
