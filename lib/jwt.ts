c39420330c6ad86c94752f9751db220da59d9a59
Iyiola-am <adekunle3317@gmail.com>
2021-01-05 03:01:26 +0100

    Refactored jwt library

--- lib/jwt.ts
+++ lib/jwt.ts
@@ -1,23 +1,23 @@
 import jwt from 'jsonwebtoken'
 import User from '../models/user';
 
-export default {
+export default class JWT {
     /**
      * Extract user id from a valid access token.
      */
-    verifyAccessToken(token: string): string|null {
+    static verifyAccessToken(token: string): string|null {
         try {
             let payload = jwt.verify(token, String(process.env.JWT_SECRET)) as {[key: string]: string};
             return payload.sub;
         } catch(e) {
             return null;
         }
-    },
+    }
 
     /**
      * Extract user id from a valid or expired access token.
      */
-    verifyExpiredAccessToken(token: string): string|null {
+    static verifyExpiredAccessToken(token: string): string|null {
         try {
             let payload = jwt.verify(token, String(process.env.JWT_SECRET)) as {[key: string]: string};
             return payload.sub;
@@ -27,12 +27,12 @@ export default {
                 return payload.sub;
             } else return null;
         }
-    },
+    }
 
     /**
      * Generate access token from a user instance
      */
-    generateAccessToken(user: User): string {
+    static generateAccessToken(user: User): string {
         return jwt.sign({
             iss: process.env.APP_URL,
             exp: (new Date).getTime() + Number(process.env.JWT_TTI),
