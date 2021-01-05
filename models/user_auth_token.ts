a99eb1e7eba210748367a9041c876296e8cb8829
Iyiola-am <adekunle3317@gmail.com>
2021-01-05 01:45:36 +0100

    Updated user auth token model

--- models/user_auth_token.ts
+++ models/user_auth_token.ts
@@ -2,7 +2,7 @@ import BaseModel from './base-model'
 import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
 import User from './user';
 
-@Entity()
+@Entity('user_auth_tokens')
 export default class UserAuthToken extends BaseModel {
   @Column('text')
   // @ts-ignore
