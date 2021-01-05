40c5f6f909947dadff8e4d6ae1e0222b0d00e586
Iyiola-am <adekunle3317@gmail.com>
2021-01-05 01:51:27 +0100

    Refactored user auth token model

--- models/user_auth_token.ts
+++ models/user_auth_token.ts
@@ -1,9 +1,20 @@
-import BaseModel from './base-model'
-import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, Column, Entity, BaseEntity, JoinColumn, ManyToOne } from 'typeorm';
 import User from './user';
 
 @Entity('user_auth_tokens')
-export default class UserAuthToken extends BaseModel {
+export default class UserAuthToken extends BaseEntity {
+  @PrimaryGeneratedColumn('increment')
+  // @ts-ignore
+  id: number;
+
+  @CreateDateColumn()
+  // @ts-ignore
+  created_at: String;
+
+  @UpdateDateColumn()
+  // @ts-ignore
+  updated_at: String;
+
   @Column('text')
   // @ts-ignore
   token: string;
