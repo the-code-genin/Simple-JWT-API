ce150a5f8df5c143801f035c885d203fe7b9597d
Iyiola-am <adekunle3317@gmail.com>
2021-01-05 03:08:27 +0100

    Refactored app errors

--- lib/errors.ts
+++ lib/errors.ts
@@ -1,10 +1,22 @@
-export default {
-    AuthenticationError: {
+export function ApplicationError(code: number, type: string, message: string) {
+    return {
         success: false,
         error: {
-            code: 401,
-            type: 'AuthenticationError',
-            message: 'User is not Authenticated'
+            code,
+            type,
+            message
         }
     }
+}
+
+export function AuthenticationError(message: string = 'User is not Authenticated!') {
+    return ApplicationError(401, 'AuthenticationError', message);
+}
+
+export function InvalidFormDataError(message: string = 'Invalid form data!') {
+    return ApplicationError(401, 'InvalidFormData', message);
+}
+
+export function ServerError(message: string = 'Server Error!') {
+    return ApplicationError(500, 'ServerError', message);
 }
