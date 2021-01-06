2173be19923a813278e99988431ef59f771994bf
Iyiola-am <adekunle3317@gmail.com>
2021-01-06 22:52:43 +0100

    Implemented home controller

---
+++ controllers/home.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+
+export default class HomeController {
+    static index(req: Request, res: Response) {
+        res.render('index.handlebars');
+    }
+}
