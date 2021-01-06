05ecfe43d1a967438184d8f51fcfcc8781af6cae
Iyiola-am <adekunle3317@gmail.com>
2021-01-06 18:02:42 +0100

    Refactored auth routes

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,13 +1,13 @@
+import { Application } from 'express';
 import AuthMiddleware from '../middleware/auth'
 import AuthController from '../controllers/auth'
-import { Application } from 'express';
 import AuthValidator from '../validators/auth';
 
 
 export default (app: Application) => {
-    app.get('/api/v1/auth', AuthMiddleware.handle, AuthController.index);
+    app.get('/api/v1/auth', AuthMiddleware, AuthController.index);
     app.post('/api/v1/auth/login', AuthValidator.login, AuthController.login);
     app.post('/api/v1/auth/signup', AuthValidator.signup, AuthController.signup);
-    app.post('/api/v1/auth/logout', AuthMiddleware.handle, AuthController.logout);
-    app.post('/api/v1/auth/refresh', AuthMiddleware.handle, AuthController.refresh);
+    app.post('/api/v1/auth/logout', AuthMiddleware, AuthController.logout);
+    app.post('/api/v1/auth/refresh', AuthMiddleware, AuthController.refresh);
 };
