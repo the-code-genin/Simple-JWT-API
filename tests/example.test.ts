6c7c83c5de20b52168f5848b44aea375518cf6ec
Iyiola-am <adekunle3317@gmail.com>
2021-01-19 20:19:15 +0100

    Refactored example test

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -1,5 +1,7 @@
 import assert from 'assert'
 
-describe('Test signup', async () => {
-    assert('1', '1');
+describe('Sample test', function() {
+    it('Passes', function () {
+        assert(1 == 1);
+    });
 });
