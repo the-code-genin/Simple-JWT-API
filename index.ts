95aa0ac774422f58689799f3bc396de1a3d40c7c
Iyiola-am <adekunle3317@gmail.com>
2021-02-20 13:06:23 +0100

    Cleaned up bootstrap file

--- index.ts
+++ index.ts
@@ -1,9 +1,8 @@
 import dotenv from 'dotenv'
 import 'reflect-metadata'
 import { createConnection } from 'typeorm'
-import express, { NextFunction, Request, Response } from 'express'
+import express from 'express'
 import corsMiddleware from 'cors'
-import {NotFoundError, ServerError} from './lib/errors'
 import routes from './routes'
 
 
@@ -31,33 +30,6 @@ import routes from './routes'
 
     // Register routes.
     routes(app);
-    
-
-    // Api 404 page
-    app.use('/api/*', (req: Request, res: Response, next: NextFunction) => {
-        res.status(404);
-        res.json(NotFoundError());
-    });
-
-    // Api 500 page
-    app.use('/api/*', function(err: Error, req: Request, res: Response, next: NextFunction){
-        console.error(err.stack);
-        res.status(500);
-        res.json(ServerError(err.message));
-    });
-
-    // General 404 page
-    app.use(function(req: Request, res: Response, next: NextFunction){
-        res.status(404);
-        res.render('404');
-    });
-
-    // General 500 page
-    app.use(function(err: Error, req: Request, res: Response, next: NextFunction){
-        console.error(err.stack);
-        res.status(500);
-        res.render('500');
-    });
 
 
     // Start server.
