77391671e23ea1df5befe1f2377ead36688016c9
Mohammed Adekunle <adekunle3317@gmail.com>
2021-07-31 02:12:04 +0100

    Refactored jwt signing

--- lib/jwt.ts
+++ lib/jwt.ts
@@ -1,31 +1,21 @@
 import jwt from 'jsonwebtoken'
 import User from '../models/user';
 
+export interface JWTData {
+    iss: string,
+    sub: number
+};
+
 export default class JWT {
     /**
      * Extract user id from a valid access token.
      */
-    static verifyAccessToken(token: string): string|null {
+    static verifyAccessToken(token: string): number | null {
         try {
-            let payload = jwt.verify(token, String(process.env.JWT_SECRET)) as {[key: string]: string};
+            let payload = jwt.verify(token, String(process.env.APP_KEY)) as JWTData;
             return payload.sub;
-        } catch(e) {
+        } catch (e) {
             return null;
-        }
-    }
-
-    /**
-     * Extract user id from a valid or expired access token.
-     */
-    static verifyExpiredAccessToken(token: string): string|null {
-        try {
-            let payload = jwt.verify(token, String(process.env.JWT_SECRET)) as {[key: string]: string};
-            return payload.sub;
-        } catch(e) {
-            if (e instanceof jwt.TokenExpiredError) {
-                let payload = jwt.decode(token) as {[key: string]: string};
-                return payload.sub;
-            } else return null;
         }
     }
 
@@ -33,10 +23,11 @@ export default class JWT {
      * Generate access token from a user instance
      */
     static generateAccessToken(user: User): string {
-        return jwt.sign({
-            iss: process.env.APP_URL,
-            exp: (new Date).getTime() + Number(process.env.JWT_TTI),
+        let data: JWTData = {
+            iss: String(process.env.APP_URL),
             sub: user.id
-        }, String(process.env.JWT_SECRET));
+        };
+
+        return jwt.sign(data, String(process.env.APP_KEY));
     }
 }
