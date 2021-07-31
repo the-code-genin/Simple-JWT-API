3a7b58c496c75ee8b4da40290bb0fe2a1abb9e18
Mohammed Adekunle <adekunle3317@gmail.com>
2021-07-31 02:16:24 +0100

    refactored auth validator

--- validators/auth.ts
+++ validators/auth.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
 import Validator from 'validatorjs'
 import User from '../models/user';
-import {InvalidFormDataError} from '../lib/errors';
+import {ConflictError, InvalidFormDataError} from '../lib/errors';
 
 export default class AuthValidator {
     static async login(req: Request, res: Response, next: NextFunction) {
@@ -20,7 +20,7 @@ export default class AuthValidator {
         let validation = new Validator(req.body, rules, messages);
     
         if (validation.fails()) {
-            res.json(InvalidFormDataError(validation.errors.first(Object.keys(validation.errors.all())[0]) as string));
+            res.status(400).json(InvalidFormDataError(validation.errors.first(Object.keys(validation.errors.all())[0]) as string));
             return;
         }
     
@@ -43,12 +43,12 @@ export default class AuthValidator {
         let validation = new Validator(req.body, rules, messages);
     
         if (validation.fails()) {
-            res.json(InvalidFormDataError(validation.errors.first(Object.keys(validation.errors.all())[0]) as string));
+            res.status(400).json(InvalidFormDataError(validation.errors.first(Object.keys(validation.errors.all())[0]) as string));
             return;
         }
 
         if (await User.count({where: {email: req.body.email}}) != 0) {
-            res.json(InvalidFormDataError('This email is not available.'));
+            res.status(409).json(ConflictError('This email is not available.'));
             return;
         }
 
