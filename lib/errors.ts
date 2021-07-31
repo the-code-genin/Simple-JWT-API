6f4838cca795675bc5adf04e934601fb7522c35a
Mohammed Adekunle <adekunle3317@gmail.com>
2021-07-31 02:11:49 +0100

    Updated location of api response and errors

--- lib/errors.ts
+++ lib/errors.ts
@@ -1,4 +1,4 @@
-import ApiResponse from "./interfaces/api-response";
+import ApiResponse from "./api-response";
 
 export function ApplicationError(code: number, type: string, message: string): ApiResponse {
     return {
@@ -11,16 +11,24 @@ export function ApplicationError(code: number, type: string, message: string): A
     }
 }
 
-export function NotFoundError(message: string = 'The resource you were looking for was not found on this server.') {
-    return ApplicationError(404, 'NotFoundError', message);
+export function InvalidFormDataError(message: string = 'Invalid form data!') {
+    return ApplicationError(400, 'InvalidFormData', message);
 }
 
 export function AuthenticationError(message: string = 'User is not Authenticated!') {
     return ApplicationError(401, 'AuthenticationError', message);
 }
 
-export function InvalidFormDataError(message: string = 'Invalid form data!') {
-    return ApplicationError(401, 'InvalidFormData', message);
+export function ForbiddenError(message: string = 'User is forbidden from accessing this resource!') {
+    return ApplicationError(403, 'ForbiddenError', message);
+}
+
+export function NotFoundError(message: string = 'The resource you were looking for was not found on this server.') {
+    return ApplicationError(404, 'NotFoundError', message);
+}
+
+export function ConflictError(message: string = 'The resource you are trying to create already exists!') {
+    return ApplicationError(409, 'Conflict', message);
 }
 
 export function ServerError(message: string = 'Server Error!') {
