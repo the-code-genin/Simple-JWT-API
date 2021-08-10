176ed3dc2d26695dce815899b96e362c4e2c1dc7
Mohammed Adekunle <adekunle3317@gmail.com>
2021-08-11 00:37:37 +0100

    Refactored upload file function

--- lib/upload-file.ts
+++ lib/upload-file.ts
@@ -1,33 +1,25 @@
 import fs from 'fs';
 import path from 'path';
+import randomString from './random-string';
 
 export default function UploadFile(fileData: string, to: string): string {
     if (!/^data\:.+\/.+\;base64\,.+$/i.test(fileData)) throw new Error('Invalid file uploaded')
 
-
     // Get the file extension
     let mimeType = fileData.split(';')[0].split(':', 2)[1];
     if (!/^.+\/.+$/.test(mimeType)) throw new Error('Invalid file mime.');
-    let fileExtension = mimeType.split('/', 2)[1];
 
-
-    // Generate a random file name
-    let randomName = "";
-    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    for (let i = 0; i < 100; i++) {
-        randomName += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    let fileName = [randomName, String(Math.floor(Math.random() * 999999)) + '.' + fileExtension]
-        .join(' ')
-        .replace(/\s+/g, '-');
-
+    // Get the image name and extension from the upload path
+    let fullFileNameArray = path.basename(to).split(".");
+    let fileExtension = String(fullFileNameArray.pop()).replace(/[^a-z0-9_]+/ig, '-');
+    let fileName = String(fullFileNameArray.join(".")).replace(/[^a-z0-9_]+/ig, '-');
+    let parsedFileName = `${fileName}-${randomString()}.${fileExtension}`;
 
     // Write the file to local storage
-    let writeStream = fs.createWriteStream(path.resolve(to, fileName));
+    let writeStream = fs.createWriteStream(path.resolve(path.dirname(to), parsedFileName));
     writeStream.write(Buffer.from(fileData.split(',', 2)[1], 'base64'));
     writeStream.close();
 
-
     // Return the uploaded file name
-    return fileName;
+    return parsedFileName;
 }
