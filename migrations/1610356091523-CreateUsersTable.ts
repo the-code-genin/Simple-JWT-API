d377b592063b9f605f77c134328aacf22a07c33a
Mohammed Adekunle <adekunle3317@gmail.com>
2021-10-05 13:13:39 +0100

    Refactored migrations

--- migrations/1610356091523-CreateUsersTable.ts
+++ migrations/1610356091523-CreateUsersTable.ts
@@ -1,4 +1,4 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateUsersTable1610356091523 implements MigrationInterface {
 
--- migrations/1610357477547-CreateUserAuthTokensTable.ts
+++ migrations/1610357477547-CreateUserAuthTokensTable.ts
@@ -1,4 +1,4 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateUserAuthTokensTable1610357477547 implements MigrationInterface {
 
