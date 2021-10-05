15439e9367930db94624b36e8fd323c2d390c34a
Mohammed Adekunle <adekunle3317@gmail.com>
2021-10-05 13:13:16 +0100

    Refactored auth validator

--- validators/auth.ts
+++ validators/auth.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
 import Validator from 'validatorjs'
 import User from '../models/user';
-import {ConflictError, InvalidFormDataError} from '../lib/errors';
+import { ConflictError, InvalidFormDataError } from '../lib/errors';
 
 export default class AuthValidator {
     static async login(req: Request, res: Response, next: NextFunction) {
@@ -9,21 +9,21 @@ export default class AuthValidator {
             email: 'required|email',
             password: 'required|min:6'
         };
-    
+
         let messages = {
             'email.required': 'Email address is required',
             'email.email': 'A valid email address is required',
             'password.required': 'Password is required',
             'password.min': 'Password must be at least 6 characters long'
         };
-    
+
         let validation = new Validator(req.body, rules, messages);
-    
+
         if (validation.fails()) {
             res.status(400).json(InvalidFormDataError(validation.errors.first(Object.keys(validation.errors.all())[0]) as string));
             return;
         }
-    
+
         next();
     }
 
@@ -32,22 +32,22 @@ export default class AuthValidator {
             email: 'required|email',
             password: 'required|min:6'
         };
-    
+
         let messages = {
             'email.required': 'Email address is required',
             'email.email': 'A valid email address is required',
             'password.required': 'Password is required',
             'password.min': 'Password must be at least 6 characters long'
         };
-    
+
         let validation = new Validator(req.body, rules, messages);
-    
+
         if (validation.fails()) {
             res.status(400).json(InvalidFormDataError(validation.errors.first(Object.keys(validation.errors.all())[0]) as string));
             return;
         }
 
-        if (await User.count({where: {email: req.body.email}}) != 0) {
+        if (await User.count({ where: { email: req.body.email } }) != 0) {
             res.status(409).json(ConflictError('This email is not available.'));
             return;
         }
