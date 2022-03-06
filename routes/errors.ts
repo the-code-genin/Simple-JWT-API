540eabc6d808f5f3f02a91ee1e37102b8d8d67fa
the-code-genin <adekunle3317@gmail.com>
2022-03-07 00:29:16 +0100

    Refactored errors

--- routes/errors.ts
+++ routes/errors.ts
@@ -2,29 +2,12 @@ import { Application, NextFunction, Request, Response } from 'express';
 import { NotFoundError, ServerError } from '../lib/errors';
 
 export default (app: Application) => {
-    // Default API 404 page
-    app.use('/api/*', (req: Request, res: Response, next: NextFunction) => {
-        res.status(404);
-        res.json(NotFoundError());
-    });
-
-    // Default API 500 page
-    app.use('/api/*', (err: Error, req: Request, res: Response, next: NextFunction) => {
-        console.error(err.stack);
-        res.status(500);
-        res.json(ServerError(err.message));
-    });
-
-    // General 404 page
     app.use((req: Request, res: Response, next: NextFunction) => {
-        res.status(404);
-        res.render('404.twig');
+        res.status(404).json(NotFoundError());
     });
 
-    // General 500 page
     app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
         console.error(err.stack);
-        res.status(500);
-        res.render('500.twig');
+        res.status(500).json(ServerError(err.message));
     });
 }
