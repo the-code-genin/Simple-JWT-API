effbdcf85d268d1257da918d382d60bcdab4ef77
the-code-genin <adekunle3317@gmail.com>
2022-03-07 02:41:33 +0100

    Refactored auth middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,14 +1,12 @@
 import jwt from '../lib/jwt'
 import { AuthenticationError } from '../lib/errors'
-import User from '../models/user'
 import { NextFunction, Request, Response } from 'express';
-import UserAuthToken from '../models/user_auth_token';
+import { checkUserForAuthToken, getUserByID, User } from '../models/users';
 
 export default async function AuthMiddleware(req: Request, res: Response, next: NextFunction) {
     let header = req.get('Authorization') as string;
     if (!/^Bearer (.+)$/i.test(header)) { // Bearer token is not present
-        res.status(401).json(AuthenticationError('User is not Authenticated'));
-        return;
+        return res.status(401).json(AuthenticationError('User is not Authenticated'));
     }
 
 
@@ -16,24 +14,21 @@ export default async function AuthMiddleware(req: Request, res: Response, next: 
     let token = (/^Bearer (.+)$/i.exec(header) as string[])[1].trim();
     let id = jwt.verifyAccessToken(token);
     if (!id) { // Invalid Bearer token
-        res.status(401).json(AuthenticationError('User is not Authenticated'));
-        return;
+        return res.status(401).json(AuthenticationError('User is not Authenticated'));
     }
 
 
     // Get the user
     let user: User | undefined;
     try {
-        user = await User.findOne({ where: { id } });
-
+        user = await getUserByID(id);
         if (user == null) {
-            throw new Error('User is not Authenticated');
-        } else if (await UserAuthToken.count({ where: { user: { id: user.id }, token } }) != 0) {
-            throw new Error('User is not Authenticated');
+            throw new Error('User is not Authenticated.');
+        } else if (await checkUserForAuthToken(Number(user.id), token)) {
+            throw new Error('Bad/Expired auth token.');
         }
     } catch (e) {
-        res.status(401).json(AuthenticationError((e as Error).message));
-        return;
+        return res.status(401).json(AuthenticationError((e as Error).message));
     }
 
     // Pass the user object to the request and execute subsequent requests
