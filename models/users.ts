626fed9de8f2843a614c24e54ecbd160b9e2d777
the-code-genin <adekunle3317@gmail.com>
2022-03-07 02:23:03 +0100

    Refactored user model

--- models/users.ts
+++ models/users.ts
@@ -1,3 +1,5 @@
+const users = global.db<User>("users");
+
 export interface User {
     id?: number;
     created_at?: Date;
@@ -10,10 +12,14 @@ export interface UserJSON extends Omit<User, "password"> {
     //
 }
 
-export function toJSON(user: User): UserJSON {
+export function userToJSON(user: User): UserJSON {
     const hiddenFields = ['password'];
     let data = Object.entries(user).filter(entry => !hiddenFields.includes(entry[0]));
     return Object.fromEntries(data);
 }
 
-export default global.db<User>("users");
+export async function getUsersCountWithEmail(email: string) {
+    return Number((await users.where("email", email).count("*", {as: "total"}))[0].total);
+}
+
+export default () => users;
