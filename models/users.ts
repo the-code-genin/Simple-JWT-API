2b226faeb8b2f56d9f215ab31b8e28210f4a98c5
the-code-genin <adekunle3317@gmail.com>
2022-03-07 03:06:15 +0100

    Refactored users model

--- models/users.ts
+++ models/users.ts
@@ -10,58 +10,69 @@ export interface UserJSON extends Omit<User, "password"> {
     //
 }
 
-export function userToJSON(user: User): UserJSON {
-    const hiddenFields = ['password'];
-    let data = Object.entries(user).filter(entry => !hiddenFields.includes(entry[0]));
-    return Object.fromEntries(data);
-}
-
-export async function getUsersCountWithEmail(email: string) {
-    const total = (await global.db<User>("users")
-        .where("email", email)
-        .count("*", { as: "total" })
-    )[0].total;
-    return Number(total);
-}
-
-export async function getUserByEmail(email: string) {
-    return global.db<User>("users")
-        .where("email", email)
-        .first();
-}
-
-export async function getUserByID(id: number) {
-    return global.db<User>("users")
-        .where("id", id)
-        .first();
-}
-
-export async function saveUser(user: User) {
-    const insertId = await global.db<User>("users")
-        .insert(user);
-    if (insertId.length != 1) throw new Error("An error occured while creating the user");
-    return getUserByID(insertId[0]) as User;
-}
-
-export async function checkUserForAuthToken(userId: number, token: string) {
-    const total = (await global.db("user_auth_tokens")
-        .where("user_id", userId)
-        .where("token", token)
-        .count("*", { as: "total" })
-    )[0].total;
-    return Number(total) != 0;
-}
-
-export async function addUserAuthToken(userId: number, token: string) {
-    if (await checkUserForAuthToken(userId, token)) {
-        throw new Error("User already has this auth token.");
+export default class Users {
+    static getQueryBuilder() {
+        return global.db<User>("users");
     }
 
-    const insertId = await global.db("user_auth_tokens").insert({
-        user_id: userId,
-        token
-    });
-    return insertId.length == 1;
-}
+    static async getUsersWithEmailCount(email: string) {
+        const total = (await Users
+            .getQueryBuilder()
+            .where("email", email)
+            .count("*", { as: "total" })
+        )[0].total;
+        return Number(total);
+    }
 
-export default () => global.db<User>("users");
+    static async getUserByEmail(email: string) {
+        return await Users
+            .getQueryBuilder()
+            .where("email", email)
+            .first();
+    }
+
+    static async getUserByID(id: number) {
+        return Users
+            .getQueryBuilder()
+            .where("id", id)
+            .first();
+    }
+
+    static async checkUserHasAuthToken(userId: number, token: string) {
+        const total = (await global
+            .db("user_auth_tokens")
+            .where("user_id", userId)
+            .where("token", token)
+            .count("*", { as: "total" })
+        )[0].total;
+        return Number(total) != 0;
+    }
+
+    static async addUserAuthToken(userId: number, token: string) {
+        if (await Users.checkUserHasAuthToken(userId, token)) {
+            throw new Error("User already has this auth token.");
+        }
+
+        const insertId = await global
+            .db("user_auth_tokens")
+            .insert({
+                user_id: userId,
+                token
+            });
+        return insertId.length == 1;
+    }
+
+    static async save(user: User) {
+        const insertId = await Users
+            .getQueryBuilder()
+            .insert(user);
+        if (insertId.length != 1) throw new Error("An error occured while creating the user");
+        return Users.getUserByID(insertId[0]) as User;
+    }
+
+    static toJSON(user: User): UserJSON {
+        const hiddenFields = ['password'];
+        let data = Object.entries(user).filter(entry => !hiddenFields.includes(entry[0]));
+        return Object.fromEntries(data);
+    }
+}
