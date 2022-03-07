14b7e1cedf7b324dcfc7f5502751e612221b5232
the-code-genin <adekunle3317@gmail.com>
2022-03-07 02:17:57 +0100

    Integrated knex

--- types/nodejs/index.d.ts
+++ types/nodejs/index.d.ts
@@ -3,7 +3,7 @@ import { Knex } from "knex";
 declare global {
     module NodeJS {
         interface Global {
-            db: Knex<any, any[]>;
+            db: Knex<any, unknown[]>;
         }
     }
 }
