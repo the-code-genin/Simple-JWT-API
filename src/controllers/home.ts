5ca280f1b725b246c2309c62c91f14a687eb1254
the-code-genin <adekunle3317@gmail.com>
2022-11-21 14:14:28 +0100

    Removed home route

--- src/controllers/home.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-import { SuccessResponse } from "../responses";
-
-export default class HomeController {
-    static async index(req: Request, res: Response) {
-        return SuccessResponse(res, {});
-    }
-}
